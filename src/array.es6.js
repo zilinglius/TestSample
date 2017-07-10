e9349a78274528bd033b2b007e265a8942b3be14
zilinglius <magstiff@qq.com>
2017-07-10 12:40:02 +0800

    unit test

---
+++ src/array.es6.js
@@ -0,0 +1,39 @@
+exports.MyArray = class MyArray {
+  constructor() {
+    this.array = [];
+  }
+
+  add(data) {
+    this.array.push(data);
+  }
+
+  remove(data) {
+    this.array = this.array.filter(current => current !== data);
+  }
+
+  search(data) {
+    const foundIndex = this.array.indexOf(data);
+    if(~foundIndex) {
+      return foundIndex;
+    }
+
+    return null;
+  }
+
+  getAtIndex(index) {
+    return this.array[index];
+  }
+
+  length() {
+    return this.array.length;
+  }
+
+  print() {
+    console.log(this.array.join(' '));
+  }
+
+  toString() {
+    return this.array.join(' ');
+  }
+};
+
---
+++ test/array.test.js
@@ -0,0 +1,39 @@
+/**
+ * Created by zilinglius on 10/07/2017.
+ */
+
+var js = require('../src/array.es6.js');
+var data = new js.MyArray();
+
+var expect = require('chai').expect;
+
+describe('MyArray', function () {
+
+  before(function () {
+
+    data.add(1);
+    data.add(2);
+    data.add(4);
+    data.add(5);
+    data.add(6);
+  });
+
+  describe('#getAtIndex()', function () {
+
+    it('should return the value when found', function () {
+      expect(data.getAtIndex(2)).to.be.equal(4);
+    });
+  });
+
+  describe('#search()', function () {
+
+    it('should return the value when found', function () {
+      expect(data.search(2)).to.be.equal(1);
+    });
+
+    it('should return null when not found', function () {
+      expect(data.search(10)).to.be.equal(null);
+    })
+  });
+
+});
